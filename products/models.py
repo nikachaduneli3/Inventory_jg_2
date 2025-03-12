2cf8b21c77db5028000daf3ba9acba328c712c26
nikachaduneli3 <testig@fds.as>
2025-03-12 18:44:55 +0400

    add gitignore

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from core.models import BaseModel
-import string
 import random
 
 
@@ -10,9 +9,16 @@ class Category(BaseModel):
     def __str__(self):
         return self.name
 
+def generate_barcode():
+    digits = '0123456789'
+    barcode = ''.join([random.choice(digits) for _ in range(13)])
+    while Item.objects.filter(barcode=barcode).exists():
+        barcode = ''.join([random.choice(digits) for _ in range(13)])
+    return barcode
+
+
 
 class Item(BaseModel):
-
     name = models.CharField(max_length=255)
     description = models.TextField()
     height = models.FloatField(null=True, blank=True)
@@ -22,15 +28,9 @@ class Item(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items')
     stock_qty = models.PositiveIntegerField(default=0)
     expiration_date = models.DateField(null=True, blank=True)
-    barcode = models.CharField(max_length=13, editable=False, unique=True)
+    barcode = models.CharField(max_length=13, unique=True,
+                               editable=False, default=generate_barcode)
 
 
-    def generate_barcode(self):
-        return ''.join(random.choice(string.digits) for _ in range(13))
-
-    def save(self, *args, **kwargs):
-        self.barcode = self.generate_barcode()
-
-        return super().save( *args, **kwargs)
     def __str__(self):
         return self.name
