3cba95e6eb5fff1e100dcc3ccbf230a53a339755
nikachaduneli3 <testig@fds.as>
2025-04-02 18:54:09 +0400

    fix

--- locations/serializers.py
+++ locations/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Location, ItemLocation
+from django.contrib.auth.models import User
+
 
 class LocationSerializer(serializers.ModelSerializer):
     location_items = serializers.StringRelatedField(many=True)
-    manager = serializers.StringRelatedField()
+    manager_name = serializers.StringRelatedField()
+    manager = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
     class Meta:
         model = Location
         fields = '__all__'
